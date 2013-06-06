4d4540ccb0367d230650bf071556d8bd2a03b670
Michael Goreanski <goreanski@gmail.com>
2013-06-06 14:05:19 +0300

    Fixed run script Fixed fastcgi script path Updated default vhost

--- build/run.sh
+++ build/run.sh
@@ -3,5 +3,7 @@
 # This script kills any currently running nginx instance
 # and starts a new one with the boilerplate config
 
+basePath=$(readlink -m $(dirname $0)/../)/src/
+
 kill `pgrep nginx` > /dev/null 2>&1 
-nginx -c $(realpath $(dirname $0)/../src/)/nginx.conf
+nginx -c $basePath/nginx.conf

6aac4ac64835a05abe12ae1d8173ed7f5c921291
Michael Gorianskii <goreanski@gmail.com>
2013-12-11 00:03:44 +0200

    Adde some debugging output to default index

--- test/www/index.php
+++ test/www/index.php
@@ -1,5 +1,11 @@
 <?php
+
 usleep(300000);
 session_start();
 header('Server: Nginx Boilerplate');
-print '<h3 style="color: #59F;">Nginx Boilerplate works (uniqid: '.uniqid().')</h3>';
+print '<h3 style="color: #59F;">Nginx Boilerplate works (uniqid: ' . uniqid() . ')</h3>';
+
+if ($_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']) {
+    print '<pre>';
+    print_r($_SERVER);
+}
